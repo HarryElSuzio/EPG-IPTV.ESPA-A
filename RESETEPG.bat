e0af8eae7d759104cdea327400aeca9763845deb
HarryElSuzio <h4rr1p3t4s@gmail.com>
2020-01-23 04:43:13 +0100

    new update 22
    new update 22

--- RESETEPG.bat
+++ RESETEPG.bat
@@ -2,10 +2,9 @@
 del guide.xml
 del guide.xml.gz@raw=true
 del EPG_TDT_MOVISTAR_TV_HarryElSuzio_auto
-wget https://raw.githubusercontent.com/dracohe/EA4GNQ/master/guide.xml.gz 
-7zG.exe e guide.xml.gz
+wget https://github.com/MPAndrew/EpgGratis/blob/master/guide.xml.gz?raw=true
+7zG.exe e guide.xml.gz@raw=true
 ren guide.xml EPG_TDT_MOVISTAR_TV_HarryElSuzio_auto
-del guide.xml.gz@raw=true.1
+del guide.xml
 del guide.xml.gz@raw=true
-del guide.xml.gz
 exit
